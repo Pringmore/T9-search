6dacad38dbed23bf9080da93fcc4f494a8c6eb24
pedro <Lakehal.company@gmail.com>
2019-04-12 15:47:00 +0200

    [T10] include the dbunit bundle and start testing on DbConnection

--- tests/first/test001.php
+++ tests/first/test001.php
@@ -2,14 +2,41 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
 
 final class ConfigurationTest extends TestCase
-{      
+{   
+    use TestCaseTrait;
+
     public function testIfConfigurationIsWorking(){
         
         $this->markTestIncomplete(
             'This test has not been implemented yet.'
         );
+    }
+
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    public function getConnection()
+    {
+        // Get the connection details
+        $servername = "mariaDB";
+        $username = "root";
+        $password = "654321";
+        $bdname = "t9_search";
+          
+        // Connect to the database
+        $pdo = new PDO("mysql:host=$servername;dbname=$bdname", $username, $password);
+        return $this->createDefaultDBConnection($pdo, "t9_search");
+    }
+
+    /**
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    public function getDataSet()
+    {
+        return $this->createFlatXMLDataSet(dirname(__FILE__).'/_files/guestbook-seed.xml');
     }
 }
 ?>
